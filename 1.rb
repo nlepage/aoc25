fac3924d0afe45b58853b2336780a5181d2fe67f
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2025-12-01 07:38:15 +0100

    day 1

---
+++ 1.rb
@@ -0,0 +1,13 @@
+f = File.open("1.input")
+lines = f.readlines.map(&:chomp)
+
+moves = lines.map{ |l| (l[0] == "L" ? -1 : 1) * l[1..].to_i }
+
+d = 50
+stops = moves.map{ |m| d = (d + m) % 100 }
+p stops.count 0
+
+d = 50
+stops = 0
+moves.each{ |m| nd = d + m; s =  m / m.abs; stops += ((d + s)..nd).step(s).count{ |x| x % 100 == 0 }; d = nd % 100 }
+p stops
