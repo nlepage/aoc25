ee11dcc91acf8299ae767993991af2db3901d4f4
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2025-12-09 21:38:46 +0100

    refactor day 1

--- 1.rb
+++ 1.rb
@@ -1,13 +1,17 @@
-f = File.open("1.input")
-lines = f.readlines.map(&:chomp)
+lines = File.open('1.input').readlines.map(&:chomp)
 
-moves = lines.map{ |l| (l[0] == "L" ? -1 : 1) * l[1..].to_i }
+moves = lines.map { |l| (l[0] == 'L' ? -1 : 1) * l[1..].to_i }
 
 d = 50
-stops = moves.map{ |m| d = (d + m) % 100 }
+stops = moves.map { |m| d = (d + m) % 100 }
 p stops.count 0
 
 d = 50
 stops = 0
-moves.each{ |m| nd = d + m; s =  m / m.abs; stops += ((d + s)..nd).step(s).count{ |x| x % 100 == 0 }; d = nd % 100 }
+moves.each do |m|
+  nd = d + m
+  s = m / m.abs
+  stops += ((d + s)..nd).step(s).count { |x| (x % 100).zero? }
+  d = nd
+end
 p stops
